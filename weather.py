24dd6eb4c449f207109b9c40433ca9335c5354c1
guy <gwuollet@stanford.edu>
2020-02-25 21:39:14 -0800

    more comments

--- weather.py
+++ weather.py
@@ -3,14 +3,17 @@ import serial
 import click
 import time
 
+# the Open Weather Map API key. A key is required for use
 API_KEY = 'f25b71fd991524f71ad191e02a39d40e'
+
+# port the Arduino listens on
 PORT = '/dev/cu.usbserial-DJ00S6Q0'
 
+#click gives you a nice interface at the command line and handles arguements
 @click.command()
 @click.option('-z', '--zip-code', default='94305', type=str, help='Zip code to return weather data for')
 @click.option('-f', '--fahrenheit', is_flag=True, default=True, help='Return temperature in fahrenheit.')
 @click.option('-c', '--celsius', is_flag=True, default=False, help='Return temperature in fahrenheit.')
-
 def get_weather(zip_code, fahrenheit, celsius):
 
     #some validation on the zip code
@@ -18,6 +21,7 @@ def get_weather(zip_code, fahrenheit, celsius):
         print("Please enter a valid zip code")
         return
 
+    #initialize the open weather map API instance
     owm = OWM(API_KEY)
 
     unit = 'fahrenheit'
@@ -32,20 +36,23 @@ def get_weather(zip_code, fahrenheit, celsius):
         temp = temperature['temp']
         high_temp = temperature['temp_max']
         low_temp = temperature['temp_min']
-
+        
         print("Temperature: {}".format(temp) )
         print("High for the day: {}".format(high_temp) )
         print("Low for the day: {}".format(low_temp) )
 
         int_temp = int(temp)
 
-        # args are port as a str and baud rate
+        # args are the port and baud rate
         arduino = serial.Serial(PORT, 115200)
-        time.wait(2) # allow the arduino to listen on the port
-        arduino.write("{}\n".format(int_temp))
+        time.sleep(2) # allow the arduino to listen on the port
+        arduino.write("{}\n".format(int_temp).encode()) #write temp to arduino
 
     except Exception as e:
        # print (e)
+
+       # The Open Weather Map API will throw an error if there is no
+       # weather data for a given zip code
        print("Open Weather Map was unable to identify the given zip code. Please enter a valid zip code.")
 
 if __name__ == '__main__':
